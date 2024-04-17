3e24b4075fac2beabdd701546e026df74b004583
ReiFionn <114220288+ReiFionn@users.noreply.github.com>
2024-04-17 16:51:48 +0100

    Got saving and loading into and from .xml file working

--- src/main/java/com/example/parisroutefinderdsaca2/GraphNode.java
+++ src/main/java/com/example/parisroutefinderdsaca2/GraphNode.java
@@ -3,26 +3,26 @@ package com.example.parisroutefinderdsaca2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GraphNode<T> {
-    public T name;
+public class GraphNode<String> {
+    public String name;
     public int nodeValue = Integer.MAX_VALUE;
     public List<GraphLink> adjList = new ArrayList<>();
     public boolean isLandmark;
     public int graphX;
     public int graphY;
 
-    public GraphNode(T name, boolean isLandmark, int graphX, int graphY) {
+    public GraphNode(String name, boolean isLandmark, int graphX, int graphY) {
         this.name = name;
         this.isLandmark = isLandmark;
         this.graphX = graphX;
         this.graphY = graphY;
     }
 
-    public void connectToNodeDirected(GraphNode<T> destNode, int cost) {
+    public void connectToNodeDirected(GraphNode<String> destNode, int cost) {
         adjList.add(new GraphLink(destNode, cost));
     }
 
-    public void connectToNodeUndirected(GraphNode<T> destNode, int cost) {
+    public void connectToNodeUndirected(GraphNode<String> destNode, int cost) {
         adjList.add(new GraphLink(destNode, cost));
         destNode.adjList.add(new GraphLink(this,cost));
     }
