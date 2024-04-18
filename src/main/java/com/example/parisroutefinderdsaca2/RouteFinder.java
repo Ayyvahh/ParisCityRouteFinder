dcdf7c2db8fe503297b1957df2d0aa2a5bae53bb
ReiFionn <114220288+ReiFionn@users.noreply.github.com>
2024-04-18 23:18:06 +0100

    added landmarks in correct places on map, added tooltip on hover over a node on the map

--- src/main/java/com/example/parisroutefinderdsaca2/RouteFinder.java
+++ src/main/java/com/example/parisroutefinderdsaca2/RouteFinder.java
@@ -7,14 +7,17 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.net.URL;
@@ -29,7 +32,7 @@ public class RouteFinder implements Initializable {
 
     /*-----------JAVAFX------------*/
 
-    public AnchorPane mapPane;
+    public AnchorPane mapPane = new AnchorPane();
     public ListView currentWaypoints;
     public Slider culturalSlid;
     public ComboBox startPointBox;
@@ -43,6 +46,7 @@ public class RouteFinder implements Initializable {
     public ImageView mapView;
     private boolean isMapPopulated = false;
     public Map<String, GraphNode<String>> graphNodes = new HashMap<>();
+    private Tooltip pillTip;
 
     @FXML
     public void scene2() throws IOException {
@@ -87,41 +91,70 @@ public class RouteFinder implements Initializable {
         Circle nodeCircle = new Circle();
         Circle nodeRing = new Circle();
 
-        nodeCircle.setCenterX(node.getGraphX());
-        nodeCircle.setCenterY(node.getGraphY());
-        nodeCircle.setRadius(5);
-
-        nodeRing.setCenterX(node.getGraphX());
-        nodeRing.setCenterY(node.getGraphY());
-        nodeRing.setRadius(8);
-        nodeRing.setFill(Color.TRANSPARENT);
-        nodeRing.setStrokeWidth(2);
-
         if (node.isLandmark()) {
+            nodeCircle.setCenterX(node.getGraphX());
+            nodeCircle.setCenterY(node.getGraphY());
+            nodeCircle.setRadius(5);
             nodeCircle.setFill(Color.RED);
+
+            nodeRing.setCenterX(node.getGraphX());
+            nodeRing.setCenterY(node.getGraphY());
+            nodeRing.setRadius(8);
+            nodeRing.setFill(Color.TRANSPARENT);
+            nodeRing.setStrokeWidth(2);
             nodeRing.setStroke(Color.RED);
         } else {
-            nodeCircle.setFill(Color.LAWNGREEN);
-            nodeRing.setStroke(Color.LAWNGREEN);
+            nodeCircle.setCenterX(node.getGraphX());
+            nodeCircle.setCenterY(node.getGraphY());
+            nodeCircle.setRadius(5);
+            nodeCircle.setFill(Color.BLACK);
         }
 
         mapPane.getChildren().addAll(nodeCircle, nodeRing);
     }
 
+    public void toolTipHover(@NotNull MouseEvent e) {
+        double mouseX = e.getX();
+        double mouseY = e.getY();
+
+        boolean onLandmark = false;
+        GraphNode<String> landmarkOn = null;
+
+        for (GraphNode<String> g : graphNodes.values()) {
+            if (g.isLandmark()) {
+                if (Math.abs(mouseX - g.getGraphX()) <= 3 && Math.abs(mouseY - g.getGraphY()) <= 3) {
+                    onLandmark = true;
+                    landmarkOn = g;
+                    break;
+                }
+            }
+        }
+
+        if (onLandmark) {
+            Main.secondPage.setCursor(Cursor.HAND);
+            pillTip.setText("Name: " + landmarkOn.getName() + ",\nLandmark X: " + landmarkOn.getGraphX() +
+                    ",\nLandmark Y: " + landmarkOn.getGraphY());
+            pillTip.show(mapView, e.getScreenX() + 10, e.getScreenY() + 10);
+        } else {
+            pillTip.hide();
+            Main.secondPage.setCursor(Cursor.DEFAULT);
+        }
+    }
+
     public void dijkstraTest() {
-//        graphNodes.put("ET", new GraphNode<>("Eiffel Tower", true,30,20)); //1,1 PLACEHOLDER, A
-//        graphNodes.put("AdT", new GraphNode<>("Arc de Triomphe", true,20,50)); //B
-//        graphNodes.put("TL", new GraphNode<>("The Louvre", true,213,250)); //C
-//        graphNodes.put("GP", new GraphNode<>("Grand Palais", false,234,134)); //D
-//        graphNodes.put("NDC", new GraphNode<>("Notre-Dame Cathedral", true,121,345)); //E
-//        graphNodes.put("CE", new GraphNode<>("Champs-Élysées", true,112,444)); //F
-//        graphNodes.put("OG", new GraphNode<>("Opera Garnier", false,444,324)); //G
-//        graphNodes.put("PdlC", new GraphNode<>("Place de la Concorde", true,312,433)); //H
-//        graphNodes.put("RS", new GraphNode<>("River Seine", true,493,123)); //I
-//        graphNodes.put("BotSC", new GraphNode<>("Basilica of the Sacré-Coeur", true,479,268)); //J
-//        graphNodes.put("TCP", new GraphNode<>("The Centre Pompidou", false,408,189)); //K
-//        graphNodes.put("PAIII", new GraphNode<>("Pont Alexandre III", true,333,378)); //L
-//        graphNodes.put("MdO", new GraphNode<>("Musée d’Orsay", true,468,498)); //M
+//        graphNodes.put("ET", new GraphNode<>("Eiffel Tower", true,126,377)); //A
+//        graphNodes.put("AdT", new GraphNode<>("Arc de Triomphe", true,80,220)); //B
+//        graphNodes.put("TL", new GraphNode<>("The Louvre", true,133,78)); //C
+//        graphNodes.put("GP", new GraphNode<>("Grand Palais", true,275,285)); //D
+//        graphNodes.put("NDC", new GraphNode<>("Notre-Dame Cathedral", true,309,270)); //E
+//        graphNodes.put("CE", new GraphNode<>("Champs-Élysées", true,358,192)); //F
+//        graphNodes.put("OG", new GraphNode<>("Opera Garnier", true,342,349)); //G
+//        graphNodes.put("PdlC", new GraphNode<>("Place de la Concorde", true,411,436)); //H
+//        graphNodes.put("RS", new GraphNode<>("River Seine", true,349,77)); //I
+//        graphNodes.put("BotSC", new GraphNode<>("Basilica of the Sacré-Coeur", true,447,31)); //J
+//        graphNodes.put("TCP", new GraphNode<>("The Centre Pompidou", true,505,355)); //K
+//        graphNodes.put("PAIII", new GraphNode<>("Pont Alexandre III", true,659,326)); //L
+//        graphNodes.put("MdO", new GraphNode<>("Musée d’Orsay", true,532,248)); //M
 //
 //        graphNodes.get("ET").connectToNodeUndirected(graphNodes.get("AdT"), 5); //adding streets between landmarks/junctions
 //        graphNodes.get("ET").connectToNodeUndirected(graphNodes.get("TL"), 9);
@@ -142,12 +175,14 @@ public class RouteFinder implements Initializable {
 //
 //        try {
 //            saveXML();
+//            System.out.println("Database saved!");
 //        } catch (Exception e) {
 //            System.err.println("Error writing from file: " + e);
 //        }
 
         try {
             loadXML();
+            System.out.println("Database loaded!");
 
             System.out.println("The cheapest path from Eiffel Tower to Musée d’Orsay");
             System.out.println("using Dijkstra's algorithm:");
@@ -169,7 +204,7 @@ public class RouteFinder implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         routeFinder = this;
-
-
+        pillTip = new Tooltip("TEST");
+        mapPane.setOnMouseMoved(this::toolTipHover);
     }
 }
