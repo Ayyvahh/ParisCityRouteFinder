b079a0a7541031048618aade93081b6cc6c9acbd
Ava <117838179+Ayyvah@users.noreply.github.com>
2024-04-28 23:00:02 +0100

    -

--- src/main/java/com/example/parisroutefinderdsaca2/Main.java
+++ src/main/java/com/example/parisroutefinderdsaca2/Main.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public static Stage mainStage;
     public static Scene mainPage, secondPage;
@@ -19,16 +24,29 @@ public class Main extends Application {
         mainStage = primaryStage; // Assign the primaryStage to the static mainStage variable
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard.fxml"));
-        mainPage = new Scene(fxmlLoader.load(), 1630, 950);
+        mainPage = new Scene(fxmlLoader.load(), 1670, 970);
 
-        // Add stylesheet to the scene
+
         mainPage.getStylesheets().add(Objects.requireNonNull(getClass().getResource("stylesheet.css")).toExternalForm()); //TODO change when done
         mainStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon1.png"))));
         mainStage.setTitle("PARIS CITY ROUTE FINDER");
         mainStage.setScene(mainPage);
+        mainStage.initStyle(StageStyle.TRANSPARENT);
         mainStage.centerOnScreen();
         mainStage.setResizable(false);
         mainStage.show();
+
+        // Make the stage draggable
+        AnchorPane root = (AnchorPane) mainPage.getRoot();
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            mainStage.setX(event.getScreenX() - xOffset);
+            mainStage.setY(event.getScreenY() - yOffset);
+        });
     }
 
     public static void switchToSecondScene() throws IOException {
